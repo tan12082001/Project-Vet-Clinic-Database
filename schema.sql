28c6262cb3383d86c7352a8b261d66ad76e0dc42
Tanmayi Manku <tanmayitanum@gmail.com>
2023-09-21 20:51:09 +0530

    Add species column

--- schema.sql
+++ schema.sql
@@ -1,5 +1,7 @@
 /* Database schema to keep the structure of entire database. */
 
+/* Project - 1: create animals table */
+
 CREATE DATABASE vet_clinic
 
 CREATE TABLE animals (
@@ -11,3 +13,8 @@ CREATE TABLE animals (
 	weight_kg DECIMAL,
 	PRIMARY KEY(id)
 );
+
+/* Project - 2: query and update animals table */
+
+ALTER TABLE animals
+ADD species VARCHAR(100);
